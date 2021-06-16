f2887eb5a65169bab7a1d3257e9c999ee818a06a
Patryk <w.patryk@o2.pl>
2021-06-16 08:23:16 +0200

    taskmanag

--- base_app/urls.py
+++ base_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import TaskListView
+from .views import TaskListView, TaskDetailView, TaskCreateView, TaskUpdate, DeleteView
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetailView.as_view(), name='single-task'),
+    path('create-task/', TaskCreateView.as_view(), name='task-create'),
+    path('update-task/<int:pk>/', TaskUpdate.as_view(), name='update-task'),
+    path('delete-task/<int:pk>/', DeleteView.as_view(), name='delete-task'),
 ]
--- base_app/views.py
+++ base_app/views.py
@@ -1,8 +1,48 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django.urls import reverse_lazy
+
 from .models import TaskModels
 
 
 class TaskListView(ListView):
     model = TaskModels
-    context_object_name = 'tasksmodel'
+    context_object_name = 'tasklist'
+    template_name = 'base_app/task_list.html'
+
+
+class TaskDetailView(DetailView):
+    model = TaskModels
+    context_object_name = 'taskdetail'
+    template_name = 'base_app/task.html'  # gdy chcemy zmienić nazwe templatki na taką jaka nam się podoba
+
+
+class TaskCreateView(CreateView):
+    model = TaskModels
+    template_name = 'base_app/task_form.html'
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):  # wykorzystano do update ten same html file list
+    model = TaskModels
+    template_name = 'base_app/task_form.html'
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class DeleteView(DeleteView):
+    model = TaskModels
+    template_name = 'base_app/task_confirm_delete.html'
+    context_object_name = 'tasks'
+    success_url = reverse_lazy('tasks')
+
+
+
+
+
+
+
